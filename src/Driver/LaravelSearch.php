d15a1bbd6fab1f2ac3703a569438688a205c47b6
simon <28737164@qq.com>
2015-03-19 10:07:08 +0800

    修复搜索Model无法正确返回问题

--- src/Driver/LaravelSearch.php
+++ src/Driver/LaravelSearch.php
@@ -2,6 +2,7 @@
 namespace Searchs\Driver;
 use Searchs\SearchBuilder;
 class LaravelSearch extends SearchBuilder {
+	
 	/* (non-PHPdoc)
 	 * @see \Searchs\SearchBuilder::where()
 	 */
@@ -10,6 +11,9 @@ class LaravelSearch extends SearchBuilder {
 		//设置默认值
 		empty($where) && $where = $this->options['where'];
 		
+		//一维转二维数组
+		count($where)===count($where,true) && $where = [$where];
+
 		foreach ($where as $values) {
 			//获取解析方法
 			$method = array_shift($values);
@@ -33,7 +37,7 @@ class LaravelSearch extends SearchBuilder {
 	 */
 	protected function limit() {
 		// TODO Auto-generated method stub
-		$this->model->take($this->options['limit'][1])->skip($this->options['limit'][0]);
+		$this->model = $this->model->take($this->options['limit'][1])->skip($this->options['limit'][0]);
 		
 		return $this->model;
 	}
@@ -43,7 +47,7 @@ class LaravelSearch extends SearchBuilder {
 	 */
 	protected function field() {
 		// TODO Auto-generated method stub
-		$this->model->select($this->options['field']);
+		$this->model = $this->model->select($this->options['field']);
 		
 		return $this->model;
 	}
@@ -53,7 +57,7 @@ class LaravelSearch extends SearchBuilder {
 	 */
 	protected function order() {
 		// TODO Auto-generated method stub
-		$this->model->orderBy($this->options['order'][0],$this->options['order'][1]);
+		$this->model = $this->model->orderBy($this->options['order'][0],$this->options['order'][1]);
 		 
 		return $this->model;
 	}
